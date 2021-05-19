345048035f42d9a797fdc3d4cd5fe203ca81df6c
mkdocker <motoihwpk@gmail.com>
2021-05-19 17:24:24 +0900

    latest

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 def mypoint(str1, num1):
-    result = "My name is %-10s, point is %5d." %(str1, num1)
-    print(result)
+    result1 = "My name is %-10s, point is %5d." %(str1, num1)
+    print(result1)
 
-mypoint("Yamada", 75)
-mypoint("Sugiyama", 1825)
+def tryal(alpha,beta):
+    result2 = ("名前は{myname}です。年齢は{myold}歳です。" .format(myname = alpha, myold = beta))
+    print(result2)
+
+tryal("Yamada", 20)
